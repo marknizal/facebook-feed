58a10b7226c0d9219f4a0753ffc19f7f5a2cdfae
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-09-08 16:51:34 +0800

    setup global css, styles and index

---
+++ src/styles/index.ts
@@ -0,0 +1,44 @@
+export const radius = {
+  sm: "0.5rem",
+  md: "0.75rem",
+  base: "1rem",
+  lg: "2rem",
+  full: "50%",
+};
+
+export const fontSize = {
+  xs: "0.8rem",
+  sm: "0.9rem",
+  base: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "1.75rem",
+};
+
+export const breakpoint = {
+  mobile: "@media only screen and (max-width: 767px)",
+  tablet: "@media only screen and (min-width: 768px) and (max-width: 1279px)",
+  desktop: "@media only screen and (min-width: 1280px)",
+};
+
+export const color = {
+  primary: "#1877F2",
+  white: "#FFFFFF",
+  black: "#000000",
+
+  transparent: "rgba(0,0,0,0)",
+  background: "#F1F1F1",
+
+  grey: "#E0E0E0",
+  grey10: "#CCCCCC",
+  grey20: "#B3B3B3",
+  grey30: "#999999",
+
+  link: "#2A9DF4",
+};
+
+export const shadow = {
+  soft: "0 2px 8px rgba(0, 0, 0, 0.05)",
+  thin: "0 2px 4px rgba(0, 0, 0, 0.08)",
+  medium: "0 4px 12px rgba(0, 0, 0, 0.12)",
+};
