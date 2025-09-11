9612c409bceab9b0ea3c3bc3356f2b227dfbf609
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-09-11 17:28:42 +0800

    add GitHub Pages deployment and optimize Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/facebook-feed/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          lodash: ["lodash"],
+        },
+      },
+    },
+  },
 })
